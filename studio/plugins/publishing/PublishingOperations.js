e6d3e1a0e1a648550477caacae575010f9a66912
Benjamin Seibert <benjamin.seibert@realcore.de>
2022-10-14 14:00:21 +0200

    client

--- studio/plugins/publishing/PublishingOperations.js
+++ studio/plugins/publishing/PublishingOperations.js
@@ -1,18 +1,21 @@
 import { useDocumentOperation } from '@sanity/react-hooks'
 import { publish } from '../../operations'
+import sanityClient from 'part:@sanity/base/client';
+
+const client = sanityClient.withConfig({
+    //TODO im Bemer projekt mit SANITY_API_VERSION ersetzen
+    apiVersion: "v2021-04-23",
+  });
 
 export function performPublishingOperation(page) {
-    console.log("perform ops", page)
     // const pageOps = useDocumentOperation(page._id, 'animal');
     // const metaOps = useDocumentOperation(`publish-metadata.${page._id}`, 'publish.metadata');
     try {
-        publish(page._id, page._rev)
-        // console.log("ops", pageOps, metaOps)
-        // pageOps.execute.publish()
-        // metaOps.execute.delete()
-        return "success"
+        // do something like this
+    client.delete(`drafts.publish-metadata${page._id.replace("drafts.","")}`)
+        // client.createOrReplace(page)
     }
     catch (err){
-        return console.log(err)
+        console.log(err)
     }
 }
