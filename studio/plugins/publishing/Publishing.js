33629de0c26dea14da3c28fcfca7d01dd977e424
AlisaKl <alisa.klein@realcore.de>
2022-10-18 10:08:34 +0200

    simple solution to update pages

--- studio/plugins/publishing/Publishing.js
+++ studio/plugins/publishing/Publishing.js
@@ -1,55 +1,62 @@
-import React, {useEffect, useState} from 'react'
-import {Box, Button, Stack} from '@sanity/ui'
-import sanityClient from 'part:@sanity/base/client';
-import { performPublishingOperation } from './PublishingOperations';
-import { PageListItem } from './components/PageListItem';
+import React, { useEffect, useState } from "react";
+import { Box, Button, Stack } from "@sanity/ui";
+import sanityClient from "part:@sanity/base/client";
+import { performPublishingOperation } from "./PublishingOperations";
+import { PageListItem } from "./components/PageListItem";
 
 const client = sanityClient.withConfig({
   //TODO im Bemer projekt mit SANITY_API_VERSION ersetzen
   apiVersion: "v2021-04-23",
 });
 
-
-
-function publishPages(pages){
-  pages.map(page => {
-    performPublishingOperation(page)
-  })
+function publishPages(pages) {
+  pages.map((page) => {
+    performPublishingOperation(page);
+  });
 }
 
 function Publishing() {
   const [pages, setPages] = useState([]);
   const query = `*[_type == 'animal' && _id in *[_type == 'publish.metadata'].documentId && _rev in *[_type == 'publish.metadata'].revId]`;
 
-  const PageContext = React.createContext(null);
-
-  const getPages = async() => {
-    client.fetch(query).then((result) => setPages(result))
-};
+  const getPages = async () => {
+    client.fetch(query).then((result) => setPages(result));
+  };
 
   useEffect(() => {
-    getPages()
+    getPages();
   }, []);
-  
-  // use useContext
 
-    return (
-      <PageContext.Provider value={pages}>
-        <Box padding={4} paddingY={5}>
-          <Stack>
-            <Stack space={4}>
-              {pages.length ? pages.map(page => {
-                return(
-                <PageListItem key={page._id} id={page._id} title={page.title}/>
-                )
-              }) : <span>No documents marked for publishing.</span>
-            }
-            </Stack>
-            <Button onClick={() => publishPages(pages)}>Publish marked pages</Button>
-          </Stack>
-        </Box>
-      </PageContext.Provider>
-    )
+  const removePage = (id) => {
+    const newPageArr = pages.filter((page) => page._id !== id);
+    setPages(newPageArr);
+  };
+
+  return (
+    <Box padding={4} paddingY={5}>
+      <Stack>
+        <Stack space={4}>
+          {pages.length ? (
+            pages.map((page) => {
+              return (
+                <PageListItem
+                  key={page._id}
+                  id={page._id}
+                  title={page.title}
+                  removePage={() => removePage(page._id)}
+                />
+              );
+            })
+          ) : (
+            <span>No documents marked for publishing.</span>
+          )}
+        </Stack>
+        <Button onClick={() => publishPages(pages)}>
+          Publish marked pages
+        </Button>
+      </Stack>
+    </Box>
+  );
 }
 
-export default Publishing
+export default Publishing;
--- studio/plugins/publishing/components/PageListItem.jsx
+++ studio/plugins/publishing/components/PageListItem.jsx
@@ -1,25 +1,27 @@
-import { useDocumentOperation } from '@sanity/react-hooks'
-import React from 'react'
+import { useDocumentOperation } from "@sanity/react-hooks";
+import React from "react";
 
-const PageListItem = ({id, title}) => {
-    const documentId = id.replace('drafts.','')
-    const pageOps = useDocumentOperation(documentId, 'animal');
-    const metaOps = useDocumentOperation(`publish-metadata.${documentId}`, 'publish.metadata');
-    
-    const onPublishHandler= () => {
-        pageOps.publish.execute()
-        metaOps.delete.execute()
-        // after publishing, execute a refetch
-        refetchPages()
-    }
-    
-  return(
+const PageListItem = ({ id, title, removePage }) => {
+  const documentId = id.replace("drafts.", "");
+  const pageOps = useDocumentOperation(documentId, "animal");
+  const metaOps = useDocumentOperation(
+    `publish-metadata.${documentId}`,
+    "publish.metadata"
+  );
+
+  const onPublishHandler = () => {
+    pageOps.publish.execute();
+    metaOps.delete.execute();
+    removePage();
+  };
+
+  return (
     <div>
       <h2>{title}</h2>
       <span>{id}</span>
       <button onClick={() => onPublishHandler()}>publish</button>
     </div>
-  )
-}
+  );
+};
 
-export {PageListItem}
+export { PageListItem };
